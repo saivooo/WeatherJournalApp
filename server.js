6615fb6982bd1f06493cef91286a40f702d77b5d
saivooo <isaiah_jennings@yahoo.com>
2022-12-09 17:23:59 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+
+//initialize the port you want to run on
+const port = 8000;
+//Set your variable named server, and pass the listen method with two arguments port and listening.
+const server = app.listen(port, listening =>{console.log(`Running on localhost: ${port}`)});
+
