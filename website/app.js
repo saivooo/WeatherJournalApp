4af863c02d0798b5694868abbbb051570c532b64
saivooo <isaiah_jennings@yahoo.com>
2022-12-11 17:13:50 -0500

    finish project. will come back later to improve UI

--- website/app.js
+++ website/app.js
@@ -1,76 +1,91 @@
 /* Global Variables */
-const response = document.getElementById('feelings').value;
-const latLongUrl = 'http://api.openweathermap.org/geo/1.0/zip?zip=';
-const tempUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+const response = document.getElementById("feelings").value;
+const latLongUrl = "http://api.openweathermap.org/geo/1.0/zip?zip=";
+const tempUrl = "https://api.openweathermap.org/data/2.5/weather?lat=";
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
-const apiKey = 'e5a0c1dddb993311e6abe63160bb80b5&units=imperial';
+const apiKey = "e5a0c1dddb993311e6abe63160bb80b5&units=imperial";
 
 // Fuction call to get weather for current zip code
-document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById("generate").addEventListener("click", performAction);
 
 // Function to get weather for current zip
-function performAction(){
-    const zipCode = document.getElementById('zip').value;
-    const currentFeelings = document.getElementById('feelings').value;
-    getLatLong(latLongUrl, zipCode, apiKey)
-    .then(function(data){
+function performAction() {
+    const zipCode = document.getElementById("zip").value;
+    const currentFeelings = document.getElementById("feelings").value;
+    //function call that calls weather api to convert user's zip code into latitude and longitude coordinates. This is needed because the api to get the temperature only recieves lat & lon
+    getLatLong(latLongUrl, zipCode, apiKey).then(function (data) {
         // console.log(data);
         const lat = data.lat;
         const lon = data.lon;
-        getTemp(tempUrl, lat, lon, apiKey)
-        .then(function(data){
+        //function call that calls weather api to return weather details, including temp from user's lat & lon coordinates
+        getTemp(tempUrl, lat, lon, apiKey).then(function (data) {
             // console.log(data);
             const temp = data.main.temp;
-            console.log(`Temperature: ${temp}\nI'm feeling: ${currentFeelings}\nDate: ${newDate}\nAPI Key: ${apiKey}`)
-            postData('/addWeatherData', {temp, newDate, currentFeelings, apiKey})
-            .then(updateUI(temp, newDate, currentFeelings))
-        })
-    })
+            console.log(
+                `Temperature: ${temp}\nI'm feeling: ${currentFeelings}\nDate: ${newDate}\nAPI Key: ${apiKey}`
+            );
+            // async function called that sends a POST request to the server side to store user data
+            postData("/addWeatherData", {
+                temp,
+                newDate,
+                currentFeelings,
+                apiKey,
+            }).then(updateUI());
+        });
+    });
 }
 
 //function to update the UI
-function updateUI(temperature, date, mood){
-    document.getElementById('date').innerHTML = `Date: ${date}`;
-    document.getElementById('temp').innerHTML = `Temperature: ${temperature}`;
-    document.getElementById('content').innerHTML = `Current Mood: ${mood}`;
-}
+const updateUI = async () => {
+    const req = await fetch("/all");
+    try {
+        const allData = await req.json();
+        console.log(allData);
+        document.getElementById("date").innerHTML = `Date: ${allData[allData.length - 1].date
+            }`;
+        document.getElementById("temp").innerHTML = `Temperature: ${Math.round(allData[allData.length - 1].temperature)
+            } degrees`;
+        document.getElementById("content").innerHTML = `Current Mood: ${allData[allData.length - 1].mood
+            }`;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
 
 //api call to get latitude & longitude of current zip. This is needed to call another api, which returns the temperature
-const getLatLong = async (url, zip, key)=>{
-    const res = await fetch(url+zip+'&appid='+key)
+const getLatLong = async (url, zip, key) => {
+    const res = await fetch(url + zip + "&appid=" + key);
     try {
         const data = await res.json();
         // console.log(data);
         return data;
-    } catch(error) {
+    } catch (error) {
         console.log("error", error);
     }
-}
+};
 
 //api call to get current temperature for user's zip
-const getTemp = async (url, lat, lon, key)=>{
-    const res = await fetch(url+lat+'&lon='+lon+'&appid='+key)
+const getTemp = async (url, lat, lon, key) => {
+    const res = await fetch(url + lat + "&lon=" + lon + "&appid=" + key);
     try {
         const data = await res.json();
         // console.log(data);
         return data;
-    } catch(error) {
+    } catch (error) {
         console.log("error", error);
     }
-}
-
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-
-const postData = async (url = '', data = {}) => {
+};
+const postData = async (url = "", data = {}) => {
     // console.log(data);
     const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
+        method: "POST",
+        credentials: "same-origin",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         // Body data type must match "Content-Type" header
         body: JSON.stringify(data),
@@ -83,4 +98,4 @@ const postData = async (url = '', data = {}) => {
     } catch (error) {
         console.log("error", error);
     }
-}
+};
